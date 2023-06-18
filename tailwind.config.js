55a16b685d80264ed006ec8c876683a0a319652d
unknown <deividcuellocuevas@gmail.com>
2023-06-17 22:06:36 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#DE00A5',
+      secundary: '#030138',
+      black: '#rgb(66,59,67)',
+      white: '#FFFFFF',
+      yellow: '#FFFF00',
+      gray: '#555555',
+      lightGray: '#CCCCCC'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
